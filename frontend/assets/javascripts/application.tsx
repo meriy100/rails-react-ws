69f75c0f88c1aafda30923ce438b032b78ab9e8d
meriy100 <ttattataa@gmail.com>
2017-08-04 16:41:30 +0900

    react router

--- frontend/assets/javascripts/application.tsx
+++ frontend/assets/javascripts/application.tsx
@@ -1,15 +1,45 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import * as injectTapEventPlugin from 'react-tap-event-plugin';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
+
 
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import MyAwesomeReactComponent from './components/MyAwesomeReactComponent';
 
 injectTapEventPlugin();
-const App = () => (
-  <MuiThemeProvider>
+
+const Home = () => (
+  <div>
     <MyAwesomeReactComponent content="hello" />
-  </MuiThemeProvider>
+  </div>
+)
+
+const About = () => (
+  <div>
+    <h2>About</h2>
+  </div>
+)
+
+const App = () => (
+  <Router>
+    <MuiThemeProvider>
+      <div>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
+          <li><Link to="/topics">Topics</Link></li>
+        </ul>
+        <hr/>
+        <Route exact path="/" component={Home}/>
+        <Route path="/about" component={About}/>
+      </div>
+    </MuiThemeProvider>
+  </Router>
 );
 
 ReactDOM.render(
